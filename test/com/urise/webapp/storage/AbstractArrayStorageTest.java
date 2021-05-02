997fb75c5810b306d910a6b2ec4654aab4ef8868
Kris <turokkristina30@gmail.com>
2021-05-02 15:00:34 +0300

    Lesson07_HW06 test refactoring

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,9 +1,26 @@
 package com.urise.webapp.storage;
 
-import static org.junit.Assert.*;
-public class AbstractArrayStorageTest extends AbstractStorageTest{
+import com.urise.webapp.exception.ExistStorageException;
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+import org.junit.Assert;
+import org.junit.Test;
 
+public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
     protected AbstractArrayStorageTest(Storage storage) {
         super(storage);
+    }
+
+    @Test(expected = StorageException.class)
+    public void saveOverflow() throws Exception {
+        try {
+            for (int i = 4; i <= AbstractArrayStorage.STORAGE_LIMIT; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            Assert.fail();
+        }
+        storage.save(new Resume());
     }
 }
---
+++ test/com/urise/webapp/storage/MapUuidStorageTest.java
@@ -0,0 +1,10 @@
+package com.urise.webapp.storage;
+
+import static org.junit.Assert.*;
+
+public class MapUuidStorageTest extends AbstractStorageTest{
+
+    protected MapUuidStorageTest(Storage storage) {
+        super(new MapUuidStorage());
+    }
+}
