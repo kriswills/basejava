3ad218c5752a8dfea56fb32d82d00a3811a742a7
Kris <turokkristina30@gmail.com>
2021-06-19 23:10:57 +0300

    Lesson16 jstl

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,14 +1,10 @@
 package com.urise.webapp.web;
 
 import com.urise.webapp.Config;
-import com.urise.webapp.model.ContactType;
-import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.Storage;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
     private Storage storage; // = Config.get().getStorage();
--- web/WEB-INF/jsp/fragments/header.jsp
+++ web/WEB-INF/jsp/fragments/header.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: turok
-  Date: 18/06/2021
-  Time: 11:18 pm
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <header><a href="resume">Управление резюме</a></header>
 <hr/>
