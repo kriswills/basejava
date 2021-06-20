8e0ed7c3803ba7b8f82e6b1268fe807fe30837ad
Kris <turokkristina30@gmail.com>
2021-06-20 18:32:38 +0300

    Lesson16 crud

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,7 +1,10 @@
 package com.urise.webapp.web;
 
 import com.urise.webapp.Config;
+import com.urise.webapp.model.ContactType;
+import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.Storage;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
@@ -16,45 +19,49 @@ public class ResumeServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        request.setCharacterEncoding("UTF-8");
+        String uuid = request.getParameter("uuid");
+        String fullName = request.getParameter("fullName");
+        Resume r = storage.get(uuid);
+        r.setFullName(fullName);
+        for (ContactType type : ContactType.values()) {
+            String value = request.getParameter(type.name());
+            if (value != null && value.trim().length() != 0) {
+                r.addContact(type, value);
+            } else {
+                r.getContacts().remove(type);
+            }
+        }
+        storage.update(r);
+        response.sendRedirect("resume");
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("resumes", storage.getAllSorted());
-        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
-
-//        request.setCharacterEncoding("UTF-8");
-//        response.setCharacterEncoding("UTF-8");
-////        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-//        response.setContentType("text/html; charset=UTF-8");
-//        Writer writer = response.getWriter();
-//        writer.write(
-//                "<html>\n" +
-//                        "<head>\n" +
-//                        "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-//                        "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
-//                        "    <title>Список всех резюме</title>\n" +
-//                        "</head>\n" +
-//                        "<body>\n" +
-//                        "<section>\n" +
-//                        "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
-//                        "    <tr>\n" +
-//                        "        <th>Имя</th>\n" +
-//                        "        <th>Email</th>\n" +
-//                        "    </tr>\n");
-//        for (Resume resume : storage.getAllSorted()) {
-//            writer.write(
-//                    "<tr>\n" +
-//                            "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
-//                            "     <td>" + resume.getContact(ContactType.MAIL) + "</td>\n" +
-//                            "</tr>\n");
-//        }
-//        writer.write("</table>\n" +
-//                "</section>\n" +
-//                "</body>\n" +
-//                "</html>\n");
-//
+        String uuid = request.getParameter("uuid");
+        String action = request.getParameter("action");
+        if (action == null) {
+            request.setAttribute("resumes", storage.getAllSorted());
+            request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+            return;
+        }
+        Resume r;
+        switch (action) {
+            case "delete":
+                storage.delete(uuid);
+                response.sendRedirect("resume");
+                return;
+            case "view":
+            case "edit":
+                r = storage.get(uuid);
+                break;
+            default:
+                throw new IllegalArgumentException("Action " + action + " is illegal");
+        }
+        request.setAttribute("resume", r);
+        request.getRequestDispatcher(
+                ("view".equals(action) ? "/WEB-INF/jsp/view.jsp" : "/WEB-INF/jsp/edit.jsp")
+        ).forward(request, response);
     }
 }
 
