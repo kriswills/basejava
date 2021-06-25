33187bf1bb9b0792a221e121a5da8c8d4a30d322
Kris <turokkristina30@gmail.com>
2021-06-25 17:33:35 +0300

    Lesson17 DB_credentials_as_resources

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -7,7 +7,7 @@ import java.io.*;
 import java.util.Properties;
 
 public class Config {
-    protected static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
+    protected static final String PROPS = ("/resumes.properties");
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;
@@ -18,13 +18,13 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
             Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS);
         }
     }
 
@@ -34,14 +34,5 @@ public class Config {
 
     public Storage getStorage() {
         return storage;
-    }
-
-    private static File getHomeDir() {
-        String prop = System.getProperty("homeDir");
-        File homeDir = new File(prop == null ? "." : prop);
-        if (!homeDir.isDirectory()) {
-            throw new IllegalStateException(homeDir + " is not directory");
-        }
-        return homeDir;
     }
 }
